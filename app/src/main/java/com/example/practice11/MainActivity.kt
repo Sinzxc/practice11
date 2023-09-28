6435553527ea228ec17755b5adbbcd7b35c0a07b
Sinzxc <nik.orig@mail.ru>
2023-09-28 21:23:28 +0300

    fix

--- app/src/main/java/com/example/practice11/MainActivity.kt
+++ app/src/main/java/com/example/practice11/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore.Images
 import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +17,19 @@ class MainActivity : AppCompatActivity() {
         val image: Int= R.drawable.two;
         val imagePlace = findViewById<ImageView>(R.id.imageView1);
         imagePlace.setImageResource(image);
+    }
+    var click=false;
+
+    fun onClickImage (view: View){
+        val imageBtn = findViewById<ImageButton>(R.id.imageButton3);
+        if(click==false)
+        {
+            imageBtn.setImageResource(R.drawable.t);
+            click=!click;
+        }else
+        {
+            imageBtn.setImageResource(R.drawable.f);
+            click=!click;
+        }
     }
 }
